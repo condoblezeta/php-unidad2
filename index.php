0d7d43b62e678960b8fc2fba6d003262e779c284
condoblezeta <flavege@outlook.com>
2021-07-10 00:38:22 -0300

    Actividad obligatoria unidad 2 - PHP

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+1-Crear un programa en PHP en el cual muestre en la página nombre, apellido, edad, hobbie, editor de código preferido, sistema operativo que utiliza.
+
+2-El programa deber ser subido a un repositorio de GitHub, pegar el link del ejercicio resuelto en el siguiente recuadro.
+*/
+
+echo "<b>Nombre:</b> " . "Flavia";
+
+echo "<br>";
+echo "<br>";
+
+echo "<b>Apellido:</b> " . "...";
+
+echo "<br>";
+echo "<br>";
+
+echo "<b>Edad:</b> " . "No pienso decirla";
+
+echo "<br>";
+echo "<br>";
+
+echo "<b>Hobby:</b> " . "Fotografía";
+
+echo "<br>";
+echo "<br>";
+
+echo "<b>Editor de código preferido:</b> " . "Antes era Sublime Text 3, ahora Visual Studio Code";
+
+echo "<br>";
+echo "<br>";
+
+echo "<b>Mi sistema operativo:</b> " . PHP_OS . " [Windows 10]";
+
+?>
+
+
